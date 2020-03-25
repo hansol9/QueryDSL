8f2aa63ac3cee567a92499e22c2c2b920bba5728
hansol9 <hansol9@gmail.com>
2020-03-24 21:57:48 -0400

    init repository

--- src/main/java/study/querydsl/QuerydsApplication.java
+++ src/main/java/study/querydsl/QuerydsApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuerydslApplication {
+public class QuerydsApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(QuerydslApplication.class, args);
+		SpringApplication.run(QuerydsApplication.class, args);
 	}
 
 }
