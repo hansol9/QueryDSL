d887bec954b738fb7ecfcc70b1f2c9d3a297c930
hansol9 <hansol9@gmail.com>
2020-04-01 11:21:30 -0400

    Adding Projections with @QueryProjections

--- src/test/java/study/querydsl/QuerydslAdvanceTest.java
+++ src/test/java/study/querydsl/QuerydslAdvanceTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -162,6 +163,18 @@ public class QuerydslAdvanceTest {
 
         for (UserDto userDto : result) {
             System.out.println("userDto = " + userDto);
+        }
+    }
+
+    @Test
+    public void findDtoByQueryProjection() throws Exception {
+        List<MemberDto> result = jpaQueryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
         }
     }
 }
